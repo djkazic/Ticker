232716f0dcdf91e411922f0bbc4a7f547094e588
Kevin Cai <kevincai96@gmail.com>
2015-09-23 01:04:02 -0400

    Setup PHP/JS geolocate

---
+++ geoupdate.php
@@ -0,0 +1,25 @@
+<!-- Send geolocation information -->
+
+<?php
+
+require('header.php');
+
+if(isset($_POST['lat']) && isset($_POST['long'])) {
+	//Look for existing record
+	$userId = getId();
+	$sres = $conn->prepare("SELECT * FROM geoloc WHERE userid = :uid");
+	$sres->bindParam(":uid", $userId);
+	$sres->execute();
+	if($sres->rowCount() > 0) {
+		//Update db
+		$gres = $conn->prepare("UPDATE geoloc SET lat = :lat, long = :long WHERE userid = :uid");
+	} else {
+		$gres = $conn->prepare("INSERT INTO geoloc VALUES ('NULL', :uid, :lat, :long)");
+	}
+	$gres->bindParam(":uid", $userId);
+	$gres->bindParam(":lat", $_POST['lat']);
+	$gres->bindParam(":long", $_POST['long']);
+	$gres->execute();
+}
+
+?>
--- header.php
+++ header.php
@@ -1,5 +1,17 @@
 <!-- Header for geo cookie updates / header redirection if no cookies -->
 
+<script src="js/geolocate.js"></script>
+<script>
+	$(document).ready(function() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(GEOprocess, GEOdeclined);
+		} else {
+			updateGeo(0, 0);
+			alert('Your browser sucks. Upgrade it.');
+		}
+	});
+</script>
+
 <?php
 require_once('includes/functions.php');
 
