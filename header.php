f7da80c257f4637e9e1b8601e686c5dacac3ac8c
Kevin Cai <kevincai96@gmail.com>
2015-09-23 08:16:57 -0400

    Impl PHP geoloc check

--- header.php
+++ header.php
@@ -2,23 +2,63 @@
 
 <script src="js/geolocate.js"></script>
 <script>
+	//TODO: switch to localStorage
 	$(document).ready(function() {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(GEOprocess, GEOdeclined);
+		$('#dieMsg').hide();
+		if(navigator.geolocation) {
+			$('#geoStat').html("Crunching numbers for your location...");
+			navigator.geolocation.getCurrentPosition(success, error);
 		} else {
-			updateGeo(0, 0);
+			$.post(
+				"geoupdate.php",
+				{lat: 0, long: 0}
+			);
+			$('#dieMsg').show();
 			alert('Your browser sucks. Upgrade it.');
 		}
 	});
+	
+	function success(position) {
+		$.post(
+			"geoupdate.php",
+			{lat: position.coords.latitude, long: position.coords.longitude},
+			function(data) {
+				$('#geoStat').html("");
+				setTimeout(function() {
+					if(document.URL.indexOf("#") == -1) {
+						url = document.URL + "#";
+						location = "#";
+						location.reload();
+					}
+				}, 2000);
+			}
+		);
+	}
+	
+	function error(error) {
+		document.getElementById('geo').innerHTML = 'Error: ' + error.message;
+	}
 </script>
+
+<div id="geoStat"></div>
 
 <?php
 require_once('includes/functions.php');
 
+//Cookie check
 if(getId() == null) {
-	//Register
 	register();
 	die();
+}
+
+//Geoloc check
+$uid = getId();
+$gcres = $conn->prepare("SELECT * FROM geoloc WHERE poster = :uid");
+$gcres->bindParam(":uid", $uid);
+$gcres->execute();
+if($gcres->rowCount() == 0) {
+	die("<div id=\"dieMsg\">Sorry, we need geo-location services to work.</div>");
+	//NEED A LANDER PAGE WITHOUT A HEADER 
 }
 
 renderNavBar();
