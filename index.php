d834f5f005a5fc33fb3275deb3073746799fa649
Kevin Cai <kevincai96@gmail.com>
2015-09-24 21:33:43 -0400

    Experimental: JS scroll load change

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 		
 		<script type="text/javascript">
 			$(window).scroll(function() {
-				if($(window).scrollTop() == $(document).height() - $(window).height()){
+				if($(window).scrollTop() > $(document).height() - $(window).height()){
 					$('#loadmoreajaxloader').show();
 					var maxId = 0;
 					$(".entries *[id]").each(function() {
