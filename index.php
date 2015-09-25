288f26f734abfb77b60ad1d2ce90b0567899f8ee
Kevin Cai <kevincai96@gmail.com>
2015-09-24 20:52:35 -0400

    Adjusted feeder call

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 		
 		<script type="text/javascript">
 			$(window).scroll(function() {
-				if($(window).scrollTop() == $(document).height() - $(window).height()) {
+				if($(window).scrollTop() + $(window).height() >= $(document).height() -300) {
 					$('#loadmoreajaxloader').show();			
 					var maxId = 0;
 					$(".entries *[id]").each(function() {
