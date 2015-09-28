cdcf3dd6e1a48dfb4dbc8682fc10c801094f42b5
Kevin Cai <kevincai96@gmail.com>
2015-09-27 22:31:56 -0400

    Impl constant geo-location

--- header.php
+++ header.php
@@ -1,4 +1,29 @@
 <!-- Header for geo cookie updates / header redirection if no cookies -->
+<html>
+
+<head>
+<script>
+	$(document).ready(function() {
+		if(navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(success, error, {enableHighAccuracy: true});
+		} else {
+			$.post(
+				"op/geoupdate.php",
+				{lat: 0, long: 0}
+			);
+		}
+	});
+
+	function success(position) {
+		$.post(
+			"op/geoupdate.php",
+			{lat: position.coords.latitude, long: position.coords.longitude}
+		);
+	}
+
+	function error(error) {}
+</script>
+</head>
 
 <?php
 require_once('includes/functions.php');
